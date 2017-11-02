5c04354ac70e0cebb0f22b05f1c3f66e05c6fbd6
msqlx <wx_liyue@163.com>
2017-11-02 23:34:28 +0800

    首页显示码上去旅行。。。。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    码上去旅行。。。。。。。。。。。
                 </div>
 
                 <div class="links">
