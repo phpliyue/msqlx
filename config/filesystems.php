a114daee4dd21a91dcea6ee91ff3f729d30de862
msqlx <wx_liyue@163.com>
2017-12-01 21:06:35 +0800

    图片上传路径配置
    Signed-off-by: msqlx <wx_liyue@163.com>

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+//            'root' => storage_path('app'),
+            'root' => public_path('/images/profile/'),
+        ],
+        'lunbotu' => [
+            'driver' => 'local',
+            'root' => public_path('/images/'),
         ],
 
         'public' => [
