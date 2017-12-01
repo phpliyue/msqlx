a26c8cf721671455ce6da17293d3653b9ff08a77
msqlx <wx_liyue@163.com>
2017-12-01 23:35:50 +0800

    auth Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/administrator';
 
     /**
      * Create a new controller instance.
