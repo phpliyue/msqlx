219dd4f1c3648fec2f1a03a64f69e78538bd233d
msqlx <wx_liyue@163.com>
2017-12-07 17:14:07 +0800

    modified web route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/','Admin\AdminController@liyue');
+Route::get('/','Web\IndexController@index');
 /*
  *
  * 后台
