e2e5f502a311c2a67f60299c1bc4ea4f3fee4432
msqlx <wx_liyue@163.com>
2017-12-26 11:08:53 +0800

    resete js

--- resources/views/temp.blade.php
+++ resources/views/temp.blade.php
@@ -423,7 +423,7 @@
         <script src="{{URL::asset('js/app.js')}}"></script>
         <script src="{{URL::asset('js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
         <script src="{{URL::asset('js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
-        {{--<script src="{{URL::asset('js/inspinia.js')}}"></script>--}}
+        <script src="{{URL::asset('js/inspinia.js')}}"></script>
 @show
 
 
