a0d03ce237c407d0fe2ed17e716ca48be36527b6
msqlx <wx_liyue@163.com>
2017-12-26 09:56:13 +0800

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,9 +3,19 @@
 @section('username'){{$user->name}}@endsection
 @section('navzero','active')
 @section('content')
-    first page
+    @foreach($data as $key => $value)
+
+
+        <div class="summernote" value="123">
+            {!! $value->detail !!}
+        </div>
+    @endforeach
 @endsection
 
 @section('js')
     @parent
+    <script src="{{URL::asset('js/plugins/summernote/summernote.min.js')}}"></script>
+    <script>
+        $('.summernote').summernote();
+    </script>
 @endsection
