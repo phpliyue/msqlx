a8f541b86f7308ee98565d24db92a61ca4f1dc71
msqlx <wx_liyue@163.com>
2018-01-04 13:19:12 +0800

    微信接口
    Signed-off-by: msqlx <wx_liyue@163.com>

---
+++ app/Http/Controllers/Wx/WeixinController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Wx;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WeixinController extends Controller
+{
+    public function getAdPic(Request $request){
+        return '11213';
+    }
+
+    public function getProInfo(Request $request){
+        dd($request);
+    }
+}
