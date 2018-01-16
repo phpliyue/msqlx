a1eb60cf15716394820af933bf4cb438a6693052
msqlx <wx_liyue@163.com>
2018-01-16 16:32:49 +0800

    user
    Signed-off-by: msqlx <wx_liyue@163.com>

--- app/Http/Controllers/Wx/WeixinController.php
+++ app/Http/Controllers/Wx/WeixinController.php
@@ -12,7 +12,6 @@ class WeixinController extends Controller
     public function getAdPic(Request $request){
         return '11213';
     }
-
     /*
      * 返回旅游信息
      * 接口地址:www.msqlx/x_getProductInfo?type=
@@ -31,5 +30,20 @@ class WeixinController extends Controller
         $id = $request->get('id');
         $Data = DB::table('product')->where('id',$id)->get();
         return json_encode($Data);
+    }
+    /*
+     * customer login get customer wx name wx headImage openid
+     * interface : https://wwww.msqlx.com/x_getCustomerLogin
+     * */
+    public function getCustomerLogin(Request $request){
+//        $wx_name = $request->get('wxName');
+//        $wx_image = $request->get('wxImage');
+//        $wx_openid = $request->get('wxOpenId');
+        $code = $request->get('code');
+//        $appid = 'wx5eb8e41173cf7ac2';
+//        $secret = 'e3bf7d92c1d041344eabf1dededfec3d';
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx5eb8e41173cf7ac2&secret=e3bf7d92c1d041344eabf1dededfec3d&js_code=$code&grant_type=authorization_code";
+        $userinfo = json_decode(file_get_contents($url),true);
+        return $userinfo;
     }
 }
