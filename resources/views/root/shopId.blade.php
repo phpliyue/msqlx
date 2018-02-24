a3c9e6189d852b1f137f4b36745fbe3a29a53535
msqlx <wx_liyue@163.com>
2018-02-24 17:31:30 +0800

    商户管理
    Signed-off-by: msqlx <wx_liyue@163.com>

--- resources/views/root/shopId.blade.php
+++ resources/views/root/shopId.blade.php
@@ -6,7 +6,6 @@
     <div class="wrapper wrapper-content animated fadeInRight">
         <div class="row">
             <div class="col-lg-2" data-toggle="modal" data-target="#myModal4">
-                {{--<a href="">--}}
                     <div class="widget style1 navy-bg">
                         <div class="row vertical-align">
                             <div class="col-xs-3">
@@ -17,8 +16,6 @@
                             </div>
                         </div>
                     </div>
-                {{--</a>--}}
-
             </div>
             <div class="modal inmodal" id="myModal4" tabindex="-1" role="dialog"  aria-hidden="true">
                 <div class="modal-dialog">
@@ -36,16 +33,13 @@
                                         {!! Form::text('shopName','',['class'=>'form-control']) !!}
                                     </div>
                                 </div>
-
                                 <div class="hr-line-dashed"></div>
-
                                 <div class="form-group has-success">
-                                    {!! Form::label('shopId','商户',['class'=>'col-sm-2 control-label']) !!}
+                                    {!! Form::label('shopId','商户ID',['class'=>'col-sm-2 control-label']) !!}
                                     <div class="col-sm-10">
                                         {!! Form::text('shopId','',['class'=>'form-control']) !!}
                                     </div>
                                 </div>
-
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group text-center">
                                     {!! Form::submit('添加','',['class'=>' text-center']) !!}
@@ -53,40 +47,22 @@
                                 {!! Form::close() !!}
                             </div>
                         </div>
-                        {{--<div class="modal-footer">--}}
-                            {{--<button type="button" class="btn btn-white" data-dismiss="modal">Close</button>--}}
-                            {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
-                        {{--</div>--}}
                     </div>
                 </div>
             </div>
-
         </div>
         <div class="row">
             <div class="col-lg-12">
                 <div class="ibox float-e-margins">
                     <div class="ibox-title">
-                        <h5>Basic Data Tables example with responsive plugin</h5>
+                        <h5>所有商户</h5>
                         <div class="ibox-tools">
                             <a class="collapse-link">
                                 <i class="fa fa-chevron-up"></i>
                             </a>
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="fa fa-wrench"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-user">
-                                <li><a href="#">Config option 1</a>
-                                </li>
-                                <li><a href="#">Config option 2</a>
-                                </li>
-                            </ul>
-                            <a class="close-link">
-                                <i class="fa fa-times"></i>
-                            </a>
                         </div>
                     </div>
                     <div class="ibox-content">
-
                         <div class="table-responsive">
                             <table class="table table-striped table-bordered table-hover dataTables-example" >
                                 <thead>
@@ -115,22 +91,19 @@
                                 <tr>
                                     <th>序号</th>
                                     <th>商户</th>
-                                    <th>商户</th>
+                                    <th>商户ID</th>
                                     <th>创建时间</th>
                                     <th>删除</th>
                                 </tr>
                                 </tfoot>
                             </table>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
-
 @section('js')
     @parent
     <script src="{{URL::asset('js/plugins/dataTables/datatables.min.js')}}"></script>
@@ -146,22 +119,17 @@
                     {extend: 'csv'},
                     {extend: 'excel', title: 'ExampleFile'},
                     {extend: 'pdf', title: 'ExampleFile'},
-
                     {extend: 'print',
                         customize: function (win){
                             $(win.document.body).addClass('white-bg');
                             $(win.document.body).css('font-size', '10px');
-
                             $(win.document.body).find('table')
                                 .addClass('compact')
                                 .css('font-size', 'inherit');
                         }
                     }
                 ]
-
             });
-
         });
-
     </script>
 @endsection
