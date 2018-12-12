a08d5231a085e65e4de52f27ca5b55754012f8de
msqlx <wx_liyue@163.com>
2018-12-12 10:44:20 +0800

    添加码上去签到 宿舍管理

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'signInAuth' => \App\Http\Middleware\SignInAuth::class,
+        'dormAuth' => \App\Http\Middleware\DormAuth::class,
     ];
 }
