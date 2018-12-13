8aebfed4f1f2751e494cc28f1063d8ae883dd201
msqlx <wx_liyue@163.com>
2018-12-13 15:04:53 +0800

    宿舍管理接口文件

---
+++ app/Http/Controllers/Api/DormController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+class DormController extends Controller
+{
+    public function saveUserInfo(Request $request)
+    {
+        $data = 1;
+        return $data;
+    }
+}
