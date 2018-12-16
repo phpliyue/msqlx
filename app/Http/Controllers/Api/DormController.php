1910bcd32206c1cb87761c9bc5b4ed936140b997
msqlx <wx_liyue@163.com>
2018-12-16 20:41:07 +0800

    修改退房时间

--- app/Http/Controllers/Api/DormController.php
+++ app/Http/Controllers/Api/DormController.php
@@ -119,6 +119,7 @@ class DormController extends Controller
                 }
                 if($res)
                 {
+                    $res = DB:table('dorm_user')->where('wx_openid',$openid)->update('out_time');
                     return json_encode(['code'=>200,'info'=>'退房成功！']);
                 }else
                 {
