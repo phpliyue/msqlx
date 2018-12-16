bc41aff0257d5b53fda6c6882ffbe7865b75766e
msqlx <wx_liyue@163.com>
2018-12-16 19:41:00 +0800

    接口

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::get('getUser','Api\SnowBallController@test');
 /*
  * 宿舍管理接口
  */
-Route::get('getOpenid','Api\GetOpenidController@getOpenid');
-Route::get('dorm_saveUserInfo','Api\DormController@saveUserInfo');
-Route::get('dorm_saveUserLogin','Api\DormController@saveUserLogin');
+Route::get('getOpenid','Api\GetOpenidController@getOpenid');//获取openid
+Route::get('dorm_saveUserInfo','Api\DormController@saveUserInfo');//存储用户基本信息，分配房间
+Route::get('dorm_saveUserLogin','Api\DormController@saveUserLogin');//记录登入平台用户
+Route::get('dorm_backRoom','Api\DormController@backRoom');//退房
+Route::get('dorm_getUserInfo','Api\DormController@getUserInfo');//在退房时显示用户基本信息
+
