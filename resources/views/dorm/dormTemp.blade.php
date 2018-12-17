1502ffea5d31ccd2dd4d90ddac6ea3bf3664e73d
msqlx <wx_liyue@163.com>
2018-12-17 12:34:39 +0800

    添加退出登入

--- resources/views/dorm/dormTemp.blade.php
+++ resources/views/dorm/dormTemp.blade.php
@@ -33,11 +33,18 @@
     </nav>
     <div id="page-wrapper" class="gray-bg dashbard-1">
         <div class="row border-bottom">
-            <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
                 <div class="navbar-header">
                     <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i>
                     </a>
                 </div>
+                <ul class="nav navbar-top-links navbar-right">
+                    <li>
+                        <a href="/dorm_logout">
+                            <i class="fa fa-sign-out"></i> 退出登入
+                        </a>
+                    </li>
+                </ul>
             </nav>
         </div>
         @section('content')
