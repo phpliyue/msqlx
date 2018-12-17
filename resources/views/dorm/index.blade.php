0604cdd51019dd1d0111e267acb3e4f18cb581b8
liyue <qmna.20@gmail.com>
2018-12-17 16:09:23 +0800

    Update index.blade.php

--- resources/views/dorm/index.blade.php
+++ resources/views/dorm/index.blade.php
@@ -76,8 +76,6 @@
                         window.location.href = '{{url('dorm_home')}}';
                     }else{
                         swal(data.info);
-                        console.log(data.info);
-                        window.location.href = '{{url('dorm_index')}}';
                     }
                 },
                 complete:function(){
