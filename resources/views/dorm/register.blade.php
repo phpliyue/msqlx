abc414b43f5170a58efc3d58fe49383ed6b808d7
msqlx <wx_liyue@163.com>
2018-12-17 13:31:01 +0800

    美化注册页面

--- resources/views/dorm/register.blade.php
+++ resources/views/dorm/register.blade.php
@@ -13,31 +13,31 @@
         <div style="text-align: center;margin-top:100px;">
             <h2>宿舍管理</h2>
         </div>
-        <div class="row" style="margin-top:5%;">
+        <div class="row" style="margin-top:10%;">
             <form class="form-horizontal">
                 {{ csrf_field() }}
                 <div class="form-group">
-                    <label for="account" class="col-md-4 control-label">账号</label>
-                    <div class="col-md-4">
+                    <label for="account" class="col-xs-2 col-xs-offset-1 col-md-4 col-md-offset-0 control-label">账号</label>
+                    <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
                         <input type="text" class="form-control" id="account" name="account" placeholder="请输入正确账号" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="password" class="col-md-4 control-label">密码</label>
-                    <div class="col-md-4">
+                    <label for="password" class="col-xs-2 col-xs-offset-1 col-md-4 col-md-offset-0 control-label">密码</label>
+                    <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
                         <input type="password" class="form-control" id="password" name="password" placeholder="请输入正确密码" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="password" class="col-md-4 control-label">电话</label>
-                    <div class="col-md-4">
+                    <label for="password" class="col-xs-2 col-xs-offset-1 col-md-4 col-md-offset-0 control-label">电话</label>
+                    <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
                         <input type="password" class="form-control" id="phone" name="phone" placeholder="请输入电话号" required>
                     </div>
                 </div>
                 <div class="form-group" style="text-align: center;">
-                    <div class="col-md-offset-4 col-md-4">
-                        <button type="submit" class="btn btn-default J_submit">注册</button>
-                        <a href="{{url('dorm_index')}}" class="btn btn-default">登录</a>
+                    <div class="col-md-offset-4 col-md-4 col-xs-10 col-xs-offset-1">
+                        <a href="{{url('dorm_index')}}" class="btn btn-default" style="width:100px;float:left;color:#08aa30;">登录</a>
+                        <button type="submit" class="btn btn-default J_submit" style="width:100px;float:right;color:#aa3400">注册</button>
                     </div>
                 </div>
             </form>
