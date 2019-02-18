a56d8d32f702ccd229d35af06172aebf5be637c4
phpliyue <qmna.20@gmail.com>
2019-02-18 10:02:25 +0800

    1

--- app/Http/Controllers/Api/EntryNoticeController.php
+++ app/Http/Controllers/Api/EntryNoticeController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class EntryNoticeController extends Controller
 {
     //入住须知
-    public function index()
+    public function index(Request $request)
     {
         $openid = $request->get('openid');
         $info = DB::table('dorm_user')->where('wx_openid',$openid)->first();
@@ -17,7 +17,12 @@ class EntryNoticeController extends Controller
         if(!empty($info)){
             $admin = DB::table('dorm_room')->where('uid',$info->uid)->value('admin');
         }
-        $content = DB::table('dorm_entrynotice')->where('admin',$admin)->value('content');
+        if(!empty($admin)){
+            $content = DB::table('dorm_entrynotice')->where('admin',$admin)->value('content');
+        }else{
+            $content = '0';
+        }
+
         return json_encode(['code'=>100,'info'=>'成功！','data'=>$content]);
     }
 }
